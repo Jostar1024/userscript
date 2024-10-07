8e41146f75759e1c0abdf0a2aecd87b0c5be5c50
Yucheng Cao <yuchengcao@tubi.tv>
2024-10-07 23:03:10 +0800

    [feature] use shadow dom

--- create-anki-card/src/index.tsx
+++ create-anki-card/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import styles from "./index.css?inline";
 import App from "./App";
 import { awaitElement, log, addLocationChangeCallback } from "./utils";
 
@@ -10,20 +11,37 @@ log("React script has successfully started");
 // so that elements can be re-inserted as a user navigates a page with
 // different routes.
 async function main() {
-    // Find <body/>. This can be any element. We wait until
-    // the page has loaded enough for that element to exist.
-    const body = await awaitElement("body");
-    const container = document.createElement("div");
-    body.appendChild(container);
-    const root = createRoot(container);
-    root.render(<App />);
+  // Find <body/>. This can be any element. We wait until
+  // the page has loaded enough for that element to exist.
+  const body = await awaitElement("body");
+  const container = document.createElement("div");
+  body.appendChild(container);
+
+  const renderIn = createShadowRoot(container, styles);
+  createRoot(renderIn).render(<App />);
 }
 
 // Call `main()` every time the page URL changes, including on first load.
 addLocationChangeCallback(() => {
-    // Greasemonkey doesn't bubble errors up to the main console,
-    // so we have to catch them manually and log them
-    main().catch((e) => {
-        log(e);
-    });
+  // Greasemonkey doesn't bubble errors up to the main console,
+  // so we have to catch them manually and log them
+  main().catch((e) => {
+    log(e);
+  });
 });
+
+// TODO: maybe use ReactDOMserver.render to render the following declaratively.
+const createShadowRoot = (container, styles) => {
+  const shadow = container.attachShadow({ mode: "open" });
+  const div = document.createElement("div");
+  const style = document.createElement("style");
+  style.innerHTML = styles;
+
+  // create the element where we would render our app
+  const renderIn = document.createElement("div");
+
+  shadow.appendChild(div);
+  div.appendChild(style);
+  div.appendChild(renderIn);
+  return renderIn;
+};
